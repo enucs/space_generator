57f734d6652b2964ce1ba88afcc99ea5c26fe06c
Jacob Barrow <barrow.jacob@gmail.com>
2019-07-27 17:07:40 +0100

    Set up simple CSS framework

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://unpkg.com/picnic">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div class="content">
+    <header>
+        <img class="logo" src="/img/logo.png" />
+        <h1>Space Generator</h1>
+    </header>
+    <form action="">
+
+    </form>
+    <hr>
+    <p>List of spaces here</p>
+</div>
+</body>
+
+</html>
